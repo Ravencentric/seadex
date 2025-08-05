a0214ed7e97955ed7c3caaf936f177dcead1787d
Ravencentric <78981416+Ravencentric@users.noreply.github.com>
2025-08-06 01:41:27 +0530

    add entries()

--- docs/api-reference/entry.md
+++ docs/api-reference/entry.md
@@ -1,1 +1,2 @@
+::: seadex.entries
 ::: seadex.SeaDexEntry
--- src/seadex/__init__.py
+++ src/seadex/__init__.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from seadex._backup import BackupFile, SeaDexBackup
 from seadex._entry import SeaDexEntry
 from seadex._enums import Tag, Tracker
@@ -7,6 +9,22 @@ from seadex._exceptions import BadBackupFileError, EntryNotFoundError, SeaDexErr
 from seadex._torrent import SeaDexTorrent
 from seadex._types import EntryRecord, File, TorrentRecord
 from seadex._version import __version__
+
+if TYPE_CHECKING:
+    from collections.abc import Iterator
+
+
+def entries() -> Iterator[EntryRecord]:  # pragma: no cover
+    """
+    Lazily iterate over all the entries in SeaDex.
+
+    This function is a simple shortcut for a common idiom. For more
+    control over the request or other means of retrieving entries, use the
+    [`SeaDexEntry`][seadex.SeaDexEntry] class directly.
+    """
+    with SeaDexEntry() as seadex_entry:
+        yield from seadex_entry.iterator()
+
 
 __all__ = (
     "BackupFile",
@@ -22,4 +40,5 @@ __all__ = (
     "TorrentRecord",
     "Tracker",
     "__version__",
+    "entries",
 )
